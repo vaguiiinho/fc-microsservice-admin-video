1818162d07d5c8f4103a21f49fe37c1e6454ccd9
vaguiiinho <avanlendario@gmail.com>
2024-06-22 21:34:17 -0300

    Criar interface definir listagem de categorias

--- src/Core/Domain/Repository/CategoryRepositoryInterface.php
+++ src/Core/Domain/Repository/CategoryRepositoryInterface.php
@@ -9,7 +9,7 @@ interface CategoryRepositoryInterface
     public function insert(Category $category): Category;
     public function findById(string $id): Category;
     public function findAll(string $filter = '', $order = 'DESC'): array;
-    public function paginate(string $filter = '', $order = 'DESC', int $page = 1, int $totalPage = 15): array;
+    public function paginate(string $filter = '', $order = 'DESC', int $page = 1, int $totalPage = 15): PaginationInterface;
     public function update(Category $category): Category;
     public function delete(string $id): bool;
     public function toCategory(object $data): Category;
