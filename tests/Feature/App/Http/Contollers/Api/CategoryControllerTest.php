a54ec120b05f3ced5b4ac6a327c80c122d29064b
vaguiiinho <avanlendario@gmail.com>
2024-08-13 00:26:01 +0000

    Criar testes e implementar cadastro de categoria

--- tests/Feature/App/Http/Contollers/Api/CategoryControllerTest.php
+++ tests/Feature/App/Http/Contollers/Api/CategoryControllerTest.php
@@ -3,19 +3,26 @@
 namespace Tests\Feature\App\Http\Contollers\Api;
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Requests\StoreCategoryRequest;
 use App\Models\Category as ModelsCategory;
 use App\Repositories\Eloquent\CategoryEloquentRepository;
+use Core\UseCase\Category\CreateCategoryUseCase;
 use Core\UseCase\Category\ListCategoriesUseCase;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\ParameterBag;
 use Tests\TestCase;
 
 class CategoryControllerTest extends TestCase
 {
     protected $repository;
+    protected $controller;
     protected function setUp(): void
     {
         $this->repository = new CategoryEloquentRepository(new ModelsCategory);
+        $this->controller = new CategoryController();
         parent::setUp();
     }
 
@@ -24,11 +31,25 @@ class CategoryControllerTest extends TestCase
 
         $useCase = new ListCategoriesUseCase($this->repository);
 
-        $controller = new CategoryController();
-        $response =  $controller->index(new Request, $useCase);
+        $response = $this->controller->index(new Request, $useCase);
 
         $this->assertInstanceOf(AnonymousResourceCollection::class, $response);
         $this->assertArrayHasKey('meta', $response->additional);
+    }
 
+    public function test_store()
+    {
+        $useCase = new CreateCategoryUseCase($this->repository);
+        $request = new StoreCategoryRequest();
+        $request->headers->set('Content-Type', 'application/json');
+
+        $request->setJson(new ParameterBag([
+            'name' => 'Test',
+        ]));
+
+        $response = $this->controller->store($request, $useCase);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CREATED, $response->status());
     }
 }
