369fc7bf53a8de88ec68edcfd12e0a6bb3cf700e
vaguiiinho <avanlendario@gmail.com>
2024-09-01 22:12:20 +0000

    Iniciando o repositório de gênero

---
+++ app/Repositories/Eloquent/GenreEloquentRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Genre as Model;
+use Core\Domain\Entity\Genre as Entity;
+use Core\Domain\Repository\GenreRepositoryInterface;
+use Core\Domain\Repository\PaginationInterface;
+
+class GenreEloquentRepository implements GenreRepositoryInterface 
+{
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function insert(Entity $genre): Entity
+    {
+
+    }
+
+    public function findById(string $id): Entity
+    {
+
+    }
+
+    public function findAll(string $filter = '', $order = 'DESC'): array
+    {
+
+    }
+
+    public function paginate(
+        string $filter = '',
+        $order = 'DESC',
+        int $page = 1,
+        int $totalPage = 15
+    ): PaginationInterface
+    {
+
+    }
+
+    public function update(Entity $genre): Entity
+    {
+
+    }
+
+    public function delete(string $id): bool
+    {
+
+    }
+}
---
+++ tests/Feature/App/Repositories/Eloquent/GenreEloquentRepositoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\App\Repositories\Eloquent;
+
+use Core\Domain\Exception\NotFoundException;
+use App\Models\Genre as Model;
+use App\Repositories\Eloquent\GenreEloquentRepository;
+use Core\Domain\Entity\Genre as Entity;
+use Core\Domain\Repository\GenreRepositoryInterface;
+use Core\Domain\Repository\PaginationInterface;
+use Tests\TestCase;
+use Throwable;
+
+class GenreEloquentRepositoryTest extends TestCase
+{
+    protected $repository;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = new GenreEloquentRepository(new Model());
+    }
+
+}
