536e5bf7ed797c280abcba76b96de55c296315c0
vaguiiinho <avanlendario@gmail.com>
2024-09-03 22:31:30 +0000

    Testes de integração usecase de criação de gênero

--- tests/Feature/Core/UseCase/Genre/CreateGenreUseCaseTest.php
+++ tests/Feature/Core/UseCase/Genre/CreateGenreUseCaseTest.php
@@ -11,13 +11,14 @@ use App\Repositories\Eloquent\{
     CategoryEloquentRepository,
 };
 use App\Repositories\Transaction\DBTransaction;
+use Core\Domain\Exception\NotFoundException;
 use Core\UseCase\DTO\Genre\Create\CreateGenreInputDto;
 use Core\UseCase\Genre\CreateGenreUseCase;
 use Tests\TestCase;
 
 class CreateGenreUseCaseTest extends TestCase
 {
-    public function test_create()
+    public function testInsert()
     {
         $genreRepository = new GenreEloquentRepository(new Genre());
         
@@ -29,14 +30,53 @@ class CreateGenreUseCaseTest extends TestCase
             $categoryRepository,
         );
 
-        $response = $useCase->execute(
+        $categories = Category::factory()->count(10)->create();
+        $categoriesId = $categories->pluck('id')->toArray();
+
+        $useCase->execute(
             new CreateGenreInputDto(
                 name: 'Test',
+                categoriesId: $categoriesId,
             )
         );
        
         $this->assertDatabaseHas('genres', [
             'name' => 'Test',
         ]);
+
+        $this->assertDatabaseCount('category_genre', 10);
+    }
+    
+    public function testExpectInsertGenreWithCategoriesIdInvalid()
+    {
+        $this->expectException(NotFoundException::class);
+
+        $genreRepository = new GenreEloquentRepository(new Genre());
+        
+        $categoryRepository = new CategoryEloquentRepository(new Category());
+
+        $useCase = new CreateGenreUseCase(
+            $genreRepository,
+            new DBTransaction(),
+            $categoryRepository,
+        );
+
+        $categories = Category::factory()->count(10)->create();
+        $categoriesId = $categories->pluck('id')->toArray();
+
+        array_push($categoriesId, 'fake_id');
+        
+        $useCase->execute(
+            new CreateGenreInputDto(
+                name: 'Test',
+                categoriesId: $categoriesId,
+            )
+        );
+       
+        $this->assertDatabaseHas('genres', [
+            'name' => 'Test',
+        ]);
+
+        $this->assertDatabaseCount('category_genre', 10);
     }
 }
