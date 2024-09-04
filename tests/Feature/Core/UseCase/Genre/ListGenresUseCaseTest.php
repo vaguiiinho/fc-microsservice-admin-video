f7150853ca7784072be89a6f8ac3c60684fb360c
vaguiiinho <avanlendario@gmail.com>
2024-09-04 21:43:00 +0000

    Testes de integração usecase de listagem de vários gêneros

---
+++ tests/Feature/Core/UseCase/Genre/ListGenresUseCaseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Genre;
+
+use App\Models\Genre as Model;
+use App\Repositories\Eloquent\GenreEloquentRepository;
+use Core\UseCase\DTO\Genre\List\ListGenresInputDto;
+use Core\UseCase\Genre\ListGenresUseCase;
+use Tests\TestCase;
+
+class ListGenresUseCaseTest extends TestCase
+{
+    public function testFindAll()
+    {
+        Model::factory()->count(100)->create();
+
+        $repository = new GenreEloquentRepository(new Model());
+
+        $useCase = new ListGenresUseCase($repository);
+
+        $response = $useCase->execute(new ListGenresInputDto());
+
+        $this->assertCount(15, $response->items);
+        $this->assertEquals(100, $response->total);
+    }
+}
