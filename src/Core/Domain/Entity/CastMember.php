d2f6cbe5c8b7aab478f54c119e1054a20b83fed4
vaguiiinho <avanlendario@gmail.com>
2024-09-05 22:50:20 +0000

    Enum na entidade de castmember

--- src/Core/Domain/Entity/CastMember.php
+++ src/Core/Domain/Entity/CastMember.php
@@ -2,15 +2,17 @@
 
 namespace Core\Domain\Entity;
 
+use Core\Domain\Enum\CastMemberType;
 use Core\Domain\ValueObject\Uuid;
 use DateTime;
 
 class CastMember
 {
     public function __construct(
-        public ?Uuid $id = null,
-        public string $name,
-        public ?DateTime $createAt = null,
+        protected ?Uuid $id = null,
+        protected string $name,
+        protected CastMemberType $type,
+        protected ?DateTime $createAt = null,
     ){
         $this->id = $this->id ?? Uuid::random();
         $this->createAt = $this->createAt ?? new DateTime();
