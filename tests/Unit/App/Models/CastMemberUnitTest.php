a5c1fb0a6ea2f73d5552fa0f85f320278261e880
vaguiiinho <avanlendario@gmail.com>
2024-09-10 20:41:07 +0000

    Criar teste do model migration e factory

---
+++ tests/Unit/App/Models/CastMemberUnitTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit\App\Models;
+
+use App\Models\CastMember;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CastMemberUnitTest extends ModelTestCase
+{
+    protected function model(): Model
+    {
+        return new CastMember();
+    }
+
+    protected function traits(): array
+    {
+        return [
+            HasFactory::class,
+            SoftDeletes::class,
+        ];
+    }
+
+    protected function fillable(): array
+    {
+        return [
+            'id',
+            'name',
+            'type',
+        ];
+    }
+
+    protected function incrementing(): bool
+    {
+        return false;
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'deleted_at' => 'datetime',
+        ];
+    }
+}
