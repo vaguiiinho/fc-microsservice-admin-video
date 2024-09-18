dd19c113ee9b53c379776a953aff8a904599a2da
vaguiiinho <avanlendario@gmail.com>
2024-09-18 23:14:20 +0000

    Criando entidade abstrata

---
+++ src/Core/Domain/Entity/Entity.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core\Domain\Entity;
+
+use Core\Domain\Notification\Notification;
+use Exception;
+
+abstract class Entity
+{
+    protected $notification;
+    
+    public function __construct()
+    {
+        $this->notification = new Notification();
+    }
+
+    public function __get($property)
+    {
+        if (isset($this->{$property}))
+            return $this->{$property};
+        
+        $className = get_class($this);
+        throw new Exception("property {$property} not found in class {$className}");
+    }
+
+    public function id() {
+        return (string) $this->id; 
+    }
+
+    public function createdAt() {
+        return $this->createdAt->format('Y-m-d H:i:s'); 
+    }
+}
