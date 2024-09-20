8af22ac65fb3cf6f267670af12dbc3fd690a9fd6
vaguiiinho <avanlendario@gmail.com>
2024-09-20 22:16:40 +0000

    TDD e definir DTO de entrada e saída do usecase de criação

--- src/Core/UseCase/Video/Create/CreateVideoUseCase.php
+++ src/Core/UseCase/Video/Create/CreateVideoUseCase.php
@@ -1,11 +1,15 @@
 <?php
 
-namespace Core\UseCase\Video;
+namespace Core\UseCase\Video\Create;
 
 use Core\Domain\Repository\VideoRepositoryInterface;
 use Core\UseCase\Interfaces\{
     FileStorageInterface,
     TransactionInterface
+};
+use Core\UseCase\Video\Create\DTO\{
+    CreateInputVideoDTO,
+    CreateOutputVideoDTO
 };
 use Core\UseCase\Video\Interfaces\VideoEventManagerInterface;
 
@@ -17,6 +21,12 @@ class CreateVideoUseCase
         protected TransactionInterface $transaction,
         protected FileStorageInterface $storage,
         protected VideoEventManagerInterface $eventManager,
-    ) {
+    ) {}
+
+    public function exec(CreateInputVideoDTO $input): CreateOutputVideoDTO
+    {
+        return new CreateOutputVideoDTO(
+           
+        );
     }
 }
--- tests/Unit/UseCase/Video/CreateVideoUseCaseUnitTest.php
+++ tests/Unit/UseCase/Video/CreateVideoUseCaseUnitTest.php
@@ -2,13 +2,17 @@
 
 namespace Tests\Unit\UseCase\Video;
 
-
+use Core\Domain\Enum\Rating;
 use Core\Domain\Repository\VideoRepositoryInterface;
 use Core\UseCase\Interfaces\{
     FileStorageInterface,
     TransactionInterface
 };
-use Core\UseCase\Video\CreateVideoUseCase as UseCase;
+use Core\UseCase\Video\Create\CreateVideoUseCase as UseCase;
+use Core\UseCase\Video\Create\DTO\{
+    CreateInputVideoDTO,
+    CreateOutputVideoDTO
+};
 use Core\UseCase\Video\Interfaces\VideoEventManagerInterface;
 use Mockery;
 use PHPUnit\Framework\TestCase;
@@ -18,12 +22,30 @@ class CreateVideoUseCaseUnitTest extends TestCase
 {
     public function test_Constructor()
     {
+        new UseCase(
+            repository: $this->createMockRepository(),
+            transaction: $this->createMockTransaction(),
+            storage: $this->createMockFileStorage(),
+            eventManager: $this->createMockEventManager(),
+        );
+
+        $this->assertTrue(true);
+    }
+
+    public function test_exec_input_output()
+    {
         $useCase = new UseCase(
             repository: $this->createMockRepository(),
             transaction: $this->createMockTransaction(),
             storage: $this->createMockFileStorage(),
             eventManager: $this->createMockEventManager(),
         );
+
+     $response =   $useCase->exec(
+            input: $this->createMockInputDto()
+        );
+
+        $this->assertInstanceOf(CreateOutputVideoDTO::class, $response);
     }
 
     private function createMockRepository()
@@ -44,5 +66,17 @@ class CreateVideoUseCaseUnitTest extends TestCase
     private function createMockEventManager()
     {
         return Mockery::mock(stdClass::class, VideoEventManagerInterface::class);
+    }
+
+    private function createMockInputDto()
+    {
+        return Mockery::mock(CreateInputVideoDTO::class, [
+            'Test Video',
+            'Test Description',
+            2022,
+            120,
+            true,
+            Rating::RATE10,
+        ]);
     }
 }
