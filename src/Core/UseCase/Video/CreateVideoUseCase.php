e745d93ddd244263b91b866ef939a90ca2970fce
vaguiiinho <avanlendario@gmail.com>
2024-09-20 21:45:30 +0000

    TDD e definir a interface de upload de arquivos

--- src/Core/UseCase/Video/CreateVideoUseCase.php
+++ src/Core/UseCase/Video/CreateVideoUseCase.php
@@ -3,14 +3,18 @@
 namespace Core\UseCase\Video;
 
 use Core\Domain\Repository\VideoRepositoryInterface;
-use Core\UseCase\Interfaces\TransactionInterface;
+use Core\UseCase\Interfaces\{
+    FileStorageInterface,
+    TransactionInterface
+};
 
 class CreateVideoUseCase
 {
 
     public function __construct(
         protected VideoRepositoryInterface $repository,
-        protected TransactionInterface $transaction
+        protected TransactionInterface $transaction,
+        protected FileStorageInterface $storage,
     ) {
     }
 }
--- tests/Unit/UseCase/Video/CreateVideoUseCaseUnitTest.php
+++ tests/Unit/UseCase/Video/CreateVideoUseCaseUnitTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Unit\UseCase\Video;
 
+
 use Core\Domain\Repository\VideoRepositoryInterface;
-use Core\UseCase\Interfaces\TransactionInterface;
+use Core\UseCase\Interfaces\{
+    FileStorageInterface,
+    TransactionInterface
+};
 use Core\UseCase\Video\CreateVideoUseCase as UseCase;
 use Mockery;
 use PHPUnit\Framework\TestCase;
@@ -11,11 +15,12 @@ use stdClass;
 
 class CreateVideoUseCaseUnitTest extends TestCase
 {
-    public function test_example()
+    public function test_Constructor()
     {
         $useCase = new UseCase(
             repository: $this->createMockRepository(),
-            transaction: $this->createMockTransaction()
+            transaction: $this->createMockTransaction(),
+            storage: $this->createMockFileStorage(),
         );
     }
 
@@ -27,5 +32,10 @@ class CreateVideoUseCaseUnitTest extends TestCase
     private function createMockTransaction()
     {
         return Mockery::mock(stdClass::class, TransactionInterface::class);
+    }
+
+    private function createMockFileStorage()
+    {
+        return Mockery::mock(stdClass::class, FileStorageInterface::class);
     }
 }
