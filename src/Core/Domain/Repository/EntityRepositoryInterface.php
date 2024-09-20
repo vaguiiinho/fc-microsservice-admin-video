82943fb260e02fce38b4854ebda3d628f423ee2c
vaguiiinho <vaguiiinho5@gmail.com>
2024-09-20 14:16:58 +0000

    Repositório de video

---
+++ src/Core/Domain/Repository/EntityRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Core\Domain\Repository;
+
+use Core\Domain\Entity\Entity;
+
+interface EntityRepositoryInterface
+{
+    public function insert(Entity $entiy): Entity;
+    public function findById(string $id): Entity;
+    public function findAll(string $filter = '', $order = 'DESC'): array;
+    public function paginate(
+        string $filter = '',
+        $order = 'DESC',
+        int $page = 1,
+        int $totalPage = 15
+    ): PaginationInterface;
+    public function update(Entity $entiy): Entity;
+    public function delete(string $id): bool;
+}
