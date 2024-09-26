121b628e6140f98f2e28c0515648b2a2d0bbf755
vaguiiinho <avanlendario@gmail.com>
2024-09-26 22:33:45 +0000

    Criar os dto de entrada de saída de atualização de vídeo

--- src/Core/UseCase/Video/Update/DTO/UpdateInputVideoDTO.php
+++ src/Core/UseCase/Video/Update/DTO/UpdateInputVideoDTO.php
@@ -7,6 +7,16 @@ use Core\Domain\Enum\Rating;
 class UpdateInputVideoDTO
 {
     public function __construct(
-       
+        public string $id,
+        public string $title,
+        public string $description,
+        public array $categories,
+        public array $genres,
+        public array $castMembers,
+        public ?array $videoFile = null,
+        public ?array $trailerFile = null,
+        public ?array $thumbFile = null,
+        public ?array $thumbHalf = null,
+        public ?array $bannerFile = null,
     ) {}
 }
--- src/Core/UseCase/Video/Update/DTO/UpdateOutputVideoDTO.php
+++ src/Core/UseCase/Video/Update/DTO/UpdateOutputVideoDTO.php
@@ -7,6 +7,20 @@ use Core\Domain\Enum\Rating;
 class UpdateOutputVideoDTO
 {
     public function __construct(
-        
+        public string $id,
+        public string $title,
+        public string $description,
+        public int $yearLaunched,
+        public int $duration,
+        public bool $opened,
+        public Rating $rating,
+        public array $categories,
+        public array $genres,
+        public array $castMembers,
+        public ?array $videoFile = null,
+        public ?array $trailerFile = null,
+        public ?array $thumbFile = null,
+        public ?array $thumbHalf = null,
+        public ?array $bannerFile = null,
     ) {}
 }
