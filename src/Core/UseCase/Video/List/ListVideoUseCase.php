40412281a36157b12491fb9c1c9c5afa6f3dbb99
vaguiiinho <avanlendario@gmail.com>
2024-09-27 22:16:34 +0000

    Finalizando o caso de uso de listagem do vídeo com tdd

--- src/Core/UseCase/Video/List/ListVideoUseCase.php
+++ src/Core/UseCase/Video/List/ListVideoUseCase.php
@@ -10,14 +10,33 @@ use Core\UseCase\Video\List\DTO\{
 
 class ListVideoUseCase
 {
-    public function __construct(
-        private VideoRepositoryInterface $repository
-    ) {
-        
+    protected $repository;
+
+    public function __construct(VideoRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
     }
 
     public function exec(ListVideoInputDto $input): ListVideoOutputDto
     {
+        $entity = $this->repository->findById($input->id);
 
+        return new ListVideoOutputDto(
+            id: $entity->id(),
+            title: $entity->title,
+            description: $entity->description,
+            yearLaunched: $entity->yearLaunched,
+            duration: $entity->duration,
+            opened: $entity->opened,
+            rating: $entity->rating,
+            categories: $entity->categoriesId,
+            genres: $entity->genresId,
+            castMembers: $entity->castMembersId,
+            videoFile: $entity->videoFile()?->filePath,
+            trailerFile: $entity->trailerFile()?->filePath,
+            thumbFile: $entity->thumbFile()?->path(),
+            thumbHalf: $entity->thumbHalf()?->path(),
+            bannerFile: $entity->bannerFile()?->path(),
+        );
     }
 }
--- tests/Unit/UseCase/Video/ListVideoUseCaseUnitTest.php
+++ tests/Unit/UseCase/Video/ListVideoUseCaseUnitTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit\UseCase\Video;
 
+use Core\Domain\Entity\Video as Entity;
+use Core\Domain\Enum\Rating;
 use Core\Domain\Repository\VideoRepositoryInterface;
 use Core\Domain\ValueObject\Uuid;
 use Core\UseCase\Video\List\ListVideoUseCase;
@@ -18,7 +20,7 @@ class ListVideoUseCaseUnitTest extends TestCase
     public function test_list()
     {
         // Arrange
-        $uuid = Uuid::random();
+        $id = Uuid::random();
 
         $useCase = new ListVideoUseCase(
             repository: $this->mockRepository()
@@ -26,9 +28,7 @@ class ListVideoUseCaseUnitTest extends TestCase
 
         // Act
         $output = $useCase->exec(
-            input: $this->mockInputDTO(
-                $uuid
-            )
+            input: $this->mockInputDTO($id)
         );
 
 
@@ -36,14 +36,32 @@ class ListVideoUseCaseUnitTest extends TestCase
         $this->assertInstanceOf(ListVideoOutputDto::class, $output);
     }
 
-    private function mockInputDTO()
+    private function mockInputDTO(string $id)
     {
-        return Mockery::mock(ListVideoInputDTO::class);
+        return Mockery::mock(ListVideoInputDTO::class, [$id]);
     }
 
     private function mockRepository()
     {
-        return Mockery::mock(stdClass::class, VideoRepositoryInterface::class);
+        $mock =  Mockery::mock(stdClass::class, VideoRepositoryInterface::class);
+
+        $mock->shouldReceive('findById')
+            ->once()
+            ->andReturn($this->getEntity());
+
+        return $mock;
+    }
+
+    private function getEntity(): Entity
+    {
+        return new Entity(
+            title: 'Test',
+            description: 'Test Description',
+            yearLaunched: 2022,
+            duration: 120,
+            opened: true,
+            rating: Rating::L,
+        );
     }
 
     protected function tearDown(): void
