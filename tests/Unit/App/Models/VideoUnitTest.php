71d553d23978771e2463227b0ff6431ac591f963
vaguiiinho <avanlendario@gmail.com>
2024-09-29 23:10:35 +0000

    Criando teste unitário do model de vídeo

---
+++ tests/Unit/App/Models/VideoUnitTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit\App\Models;
+
+use App\Models\Video;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class VideoUnitTest extends ModelTestCase
+{
+    protected function model(): Model
+    {
+        return new Video();
+    }
+
+    protected function traits(): array
+    {
+        return [
+            HasFactory::class,
+            SoftDeletes::class,
+        ];
+    }
+
+    protected function fillable(): array
+    {
+        return [
+            'id',
+            'title',
+            'description',
+            'year_launched',
+            'opened',
+            'rating',
+            'duration',
+        ];
+    }
+
+    protected function incrementing(): bool
+    {
+        return false;
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'is_active' => 'boolean',
+            'deleted_at' => 'datetime',
+        ];
+    }
+}
