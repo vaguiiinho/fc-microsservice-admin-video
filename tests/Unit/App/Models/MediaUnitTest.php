366ed21932790ce1bcc690ada7c9c8a033ce775c
vaguiiinho <avanlendario@gmail.com>
2024-10-02 15:16:56 +0000

    Teste unitário do model media

---
+++ tests/Unit/App/Models/MediaUnitTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Unit\App\Models;
+
+use App\Models\Media;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MediaUnitTest extends ModelTestCase
+{
+    protected function model(): Model
+    {
+        return new Media();
+    }
+
+    protected function traits(): array
+    {
+        return [
+            HasFactory::class,
+        ];
+    }
+
+    protected function fillable(): array
+    {
+        return [
+            'file_path',
+            'encoded_path',
+            'media_status',
+            'type',
+        ];
+    }
+
+    protected function incrementing(): bool
+    {
+        return false;
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'is_active' => 'boolean',
+            'deleted_at' => 'datetime',
+        ];
+    }
+}
