65d47aab5c51009606eb82608b3a03c8b22c2bec
vaguiiinho <avanlendario@gmail.com>
2024-10-06 18:01:18 +0000

    Testar use case com arquivos

--- tests/Feature/Core/UseCase/Video/Create/CreateVideoUseCaseTest.php
+++ tests/Feature/Core/UseCase/Video/Create/CreateVideoUseCaseTest.php
@@ -22,6 +22,7 @@ use Core\UseCase\Interfaces\{
 use Core\UseCase\Video\Create\CreateVideoUseCase;
 use Core\UseCase\Video\Create\DTO\CreateInputVideoDTO;
 use Core\UseCase\Video\Interfaces\VideoEventManagerInterface;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class CreateVideoUseCaseTest extends TestCase
@@ -42,6 +43,15 @@ class CreateVideoUseCaseTest extends TestCase
         $genresIds = Genre::factory()->count(3)->create()->pluck('id')->toArray();
         $castMemberIds = CastMember::factory()->count(3)->create()->pluck('id')->toArray();
 
+        $fakeFile = UploadedFile::fake()->create('video.mp4', 1, 'video.mp4');
+
+        $file = [
+            'tmp_name' => $fakeFile->getPathName(),
+            'name' => $fakeFile->getFileName(),
+            'type' => $fakeFile->getMimeType(),
+            'error' => $fakeFile->getError(),
+        ];
+
         $input = new CreateInputVideoDTO(
             title: 'Test Video',
             description: 'Test Description',
@@ -52,6 +62,7 @@ class CreateVideoUseCaseTest extends TestCase
             categories: $categoriesIds,
             genres: $genresIds,
             castMembers: $castMemberIds,
+            videoFile: $file,
         );
 
         $response = $useCase->exec(input: $input);
@@ -66,5 +77,11 @@ class CreateVideoUseCaseTest extends TestCase
         $this->assertCount(count($input->categories), $response->categories);
         $this->assertCount(count($input->genres), $response->genres);
         $this->assertCount(count($input->castMembers), $response->castMembers);
+
+        $this->assertNotNull($response->videoFile);
+        $this->assertNull($response->trailerFile);
+        $this->assertNull($response->thumbFile);
+        $this->assertNull($response->thumbHalf);
+        $this->assertNull($response->bannerFile);
     }
 }
