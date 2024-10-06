a5c270f9e6d4cab156919b28be53ddd378f16dec
vaguiiinho <avanlendario@gmail.com>
2024-10-06 14:34:35 +0000

    Converter files para uploadfile do laravel

--- app/Services/Storage/FileStorate.php
+++ app/Services/Storage/FileStorate.php
@@ -3,6 +3,8 @@
 namespace App\Services\Storage;
 
 use Core\UseCase\Interfaces\FileStorageInterface;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class FileStorage implements FileStorageInterface
 {
@@ -12,13 +14,25 @@ class FileStorage implements FileStorageInterface
      */
     public function store(string $path, array $file): string
     {
+        $contents = $this->convertFileToLaravelFile($file);
 
+        // return $contents->store($path);
+        return Storage::put($path, $contents);
     }
     
     public function delete(string $path)
     {
-
+        Storage::delete($path);
     }
-    
+
+    protected function convertFileToLaravelFile(array $file): UploadedFile
+    {
+        return new UploadedFile(
+           path: $file['tmp_name'],
+           originalName: $file['name'],
+           mimeType: $file['type'],
+           error: $file['error'],
+        );
+    }
 }
 
