ddb20ae2a62646d85319d8b3376079188ed8e3b7
vaguiiinho <avanlendario@gmail.com>
2024-10-16 23:29:00 +0000

    Teste de integração de casos de uso de deletar video testar exception

--- tests/Feature/Core/UseCase/Video/DeleteVideoUseCaseTest.php
+++ tests/Feature/Core/UseCase/Video/DeleteVideoUseCaseTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Core\UseCase\Video;
 
 use App\Models\Video;
+use Core\Domain\Exception\NotFoundException;
 use Core\Domain\Repository\VideoRepositoryInterface;
 use Core\UseCase\Video\Delete\DeleteVideoUseCase;
 use Core\UseCase\Video\Delete\DTO\DeleteVideoInputDto;
@@ -21,5 +22,17 @@ class DeleteVideoUseCaseTest extends TestCase
         $response = $useCase->execute(new DeleteVideoInputDto(id: $video->id));
 
         $this->assertTrue($response->success);
+    }
+
+    public function test_delete_not_found()
+    {
+        $this->expectException(NotFoundException::class);
+        $this->expectExceptionMessage('Video not found');
+
+        $useCase = new DeleteVideoUseCase(
+            $this->app->make(VideoRepositoryInterface::class),
+        );
+
+        $useCase->execute(new DeleteVideoInputDto('fake_id'));
     }
 }
