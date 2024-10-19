f25b44f17283165b95cae0e1fee2418ac5ab7fea
vaguiiinho <avanlendario@gmail.com>
2024-10-19 15:44:54 +0000

    Testar exception de transactions

--- tests/Feature/Core/UseCase/Video/CreateVideoUseCaseTest.php
+++ tests/Feature/Core/UseCase/Video/CreateVideoUseCaseTest.php
@@ -5,7 +5,11 @@ namespace Tests\Feature\Core\UseCase\Video\Create;
 use Core\Domain\Enum\Rating;
 use Core\UseCase\Video\Create\CreateVideoUseCase;
 use Core\UseCase\Video\Create\DTO\CreateInputVideoDTO;
+use Exception;
+use Illuminate\Database\Events\TransactionBeginning;
+use Illuminate\Support\Facades\Event;
 use Tests\Feature\Core\UseCase\Video\BaseVideoUseCase;
+use Throwable;
 
 class CreateVideoUseCaseTest extends BaseVideoUseCase
 {
@@ -39,5 +43,24 @@ class CreateVideoUseCaseTest extends BaseVideoUseCase
             thumbHalf: $thumbHalf,
             bannerFile: $bannerFile,
         );
+    }
+
+    /**
+     * @test
+     */
+    public function transactionExcepition()
+    {
+        Event::listen(TransactionBeginning::class, function () {
+            throw new Exception('Begin transaction');
+        });
+
+        try {
+            $sut = $this->makeSut();
+            $input = $this->inputDto();
+            $sut->exec($input);
+            $this->assertTrue(false);
+        } catch (Throwable $th) {
+            $this->assertDatabaseCount('videos', 0);
+        }
     }
 }
