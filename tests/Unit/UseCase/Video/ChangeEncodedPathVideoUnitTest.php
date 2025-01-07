c40c6bf15e75034414eecd642fff4d36ddc494f6
vaguiiinho <avanlendario@gmail.com>
2025-01-07 00:23:04 +0000

    Mock exception no teste unitário

--- tests/Unit/UseCase/Video/ChangeEncodedPathVideoUnitTest.php
+++ tests/Unit/UseCase/Video/ChangeEncodedPathVideoUnitTest.php
@@ -4,10 +4,13 @@ namespace Tests\Unit\UseCase\Video;
 
 use Core\Domain\Entity\Video as Entity;
 use Core\Domain\Enum\Rating;
+use Core\Domain\Exception\NotFoundException;
 use Core\Domain\Repository\VideoRepositoryInterface;
-use Core\UseCase\Video\ChangeEncoded\ChangeEncodedPathVideo;
-use Core\UseCase\Video\ChangeEncoded\DTO\ChangeEncodedVideoDTO;
-use Core\UseCase\Video\ChangeEncoded\DTO\ChangeEncodedVideoOutputDTO;
+use Core\UseCase\Video\ChangeEncoded\{
+    ChangeEncodedPathVideo,
+    DTO\ChangeEncodedVideoDTO,
+    DTO\ChangeEncodedVideoOutputDTO
+};
 use Mockery;
 use PHPUnit\Framework\TestCase;
 use stdClass;
@@ -38,6 +41,32 @@ class ChangeEncodedPathVideoUnitTest extends TestCase
         $response = $useCase->exec(input: $input);
 
         $this->assertInstanceOf(ChangeEncodedVideoOutputDTO::class, $response);
+
+        Mockery::close();
+    }
+
+    public function testExceptionRepository()
+    {
+        $this->expectException(NotFoundException::class);
+
+        $input = new ChangeEncodedVideoDTO(
+            id: 'id-video',
+            encodedPath: 'path/video_encoded.ext',
+        );
+
+        $mockRepository = Mockery::mock(stdClass::class, VideoRepositoryInterface::class);
+        $mockRepository->shouldReceive('findById')
+                        ->times(1)
+                        ->with($input->id)
+                        ->andThrow(new NotFoundException('Not Found Video'));
+        $mockRepository->shouldReceive('updateMedia')
+                        ->times(0);
+
+        $useCase = new ChangeEncodedPathVideo(
+            repository: $mockRepository
+        );
+
+        $response = $useCase->exec(input: $input);
 
         Mockery::close();
     }
